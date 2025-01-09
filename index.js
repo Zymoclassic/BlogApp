2914fa4d2d8157df235d6aa9d53816ee8349c33c
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2025-01-09 10:26:44 +0100

    Started my app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(process.env.PORT))
+.then(() => console.log(`Connection successful, please check port ${process.env.PORT}!`))
+.catch((err) => console.log(err));
+
+module.exports = app;
